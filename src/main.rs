7b0ef3d7cc43508a21014ea6b1a321745d13fa97
Dev-Myna <var.devmyna@gmail.com>
2023-10-08 01:44:19 -0300

    Add old scripts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+use clap::{command, Arg, Command};
+
+use crate::subcommands::*;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .author("Dev Myna <var.devmyna@gmail.com>")
+        .about("A manager of my workflow and the better programs that I use.")
+        .subcommand(
+            Command::new("daemon").about("Manage daemon processes").arg(
+                Arg::new("ACTION")
+                    .help("Action to perform <start|stop>")
+                    .required(true)
+                    .index(1),
+            ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("daemon", matches)) => {
+            daemon(matches);
+        }
+    }
 }
