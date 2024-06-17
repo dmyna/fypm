eb457ae63fc87a3641d042512b55a1cabd7e9196
Dev Myna <var.devmyna@gmail.com>
2024-06-17 17:49:51 -0300

    Adjustment(files): Change old scripts directory

--- scripts/fypm/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CONTAINER_NAME="fypm_test"
-
-sudo -v
-
-sudo docker cp "$PWD" $CONTAINER_NAME:/app # -> docker
-
-# run cargo test in container
-sudo docker exec -it $CONTAINER_NAME -sh -c "cargo test"
