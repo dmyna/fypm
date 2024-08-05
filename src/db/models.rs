d2d01aaa17285c2f2ab0cb4234f48fb8cc870fc4
Dev Myna <var.devmyna@gmail.com>
2024-08-05 18:49:04 -0300

    feat(database): add models and schema

---
+++ src/db/models.rs
@@ -0,0 +1,20 @@
+use diesel::{prelude::{Insertable, Queryable}, Selectable};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations};
+
+use crate::db::schema::worktimes;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = worktimes)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct Worktime {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub style: String,
+    pub start_time: String,
+    pub end_time: String,
+    pub polybar_background: String,
+    pub polybar_foreground: String,
+}
