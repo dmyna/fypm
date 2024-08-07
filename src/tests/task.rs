b930b5927a94003b78e7573fbe97a14dfe01d58c
Dev Myna <var.devmyna@gmail.com>
2024-08-07 11:40:15 -0300

    test(task): create base of `task_done` testing

--- src/tests/task.rs
+++ src/tests/task.rs
@@ -1,31 +1,52 @@
 #[cfg(test)]
-use crate::subcommands::task;
+pub mod done {
+    use std::process::{Command, Stdio};
+    use std::{thread::sleep, time::Duration};
 
-#[cfg(test)]
-fn mock_tasks() {
-    use std::process::Command;
+    use crate::subcommands::task;
+    use crate::utils::get::get_json_by_filter;
+    use super::super::lib::mock::Mock;
 
-    Command::new("task")
-        .args(["add", "Test1"])
-        .output()
-        .unwrap();
+    #[test]
+    pub fn simple_done() {
+        // Prepair test
+        let mock = Mock;
+        {
+            mock.mock_db();
+            mock.mock_tasks();
 
-    Command::new("task")
-        .args(["add", "Test2"])
-        .output()
-        .unwrap();
+            Command::new("task")
+                .args(["1", "start"])
+                .stdout(Stdio::inherit())
+                .stderr(Stdio::inherit())
+                .output()
+                .unwrap();
 
-    Command::new("task")
-        .args(["add", "Test3"])
-        .output()
-        .unwrap();
-}
+            sleep(Duration::from_millis(1000));
+        }
 
-#[test]
-pub fn test_done() {
-    mock_tasks();
+        // Test
+        {
+            task::task_done(
+                &Some("2,3,4".to_string()),
+                &None,
+                &None,
+                &true,
+                &false,
+                &false,
+            )
+            .unwrap();
 
-    let execute = task::task_done(&Some("1,2,3".to_string()), &None);
+            let tasks = get_json_by_filter("description.is:'Test'", None).unwrap();
 
-    assert!(execute.is_ok());
+            assert_eq!(tasks.len(), 3);
+            assert_eq!(
+                tasks
+                    .iter()
+                    .filter(|task| task.status == "completed")
+                    .count(),
+                3
+            );
+        }
+    }
 }
