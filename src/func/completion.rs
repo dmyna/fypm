b9bf0fae965db95598bfdaa6cb13df251c4464b6
Dev Myna <var.devmyna@gmail.com>
2024-10-31 13:52:54 -0300

    feat(completion): create base code of project completion

--- src/func/completion.rs
+++ src/func/completion.rs
@@ -1,7 +1,38 @@
 use clap::CommandFactory;
 use clap::ValueEnum;
 use clap_complete::{aot, CompletionCandidate};
+use std::process::Command;
+
 use crate::values::{self, err::FypmError};
+
+pub fn project(current: &std::ffi::OsStr) -> Vec<CompletionCandidate> {
+    let mut completions = vec![];
+    let Some(current) = current.to_str() else {
+        return completions;
+    };
+
+    let get_candidates = Command::new("task")
+        .arg("_projects")
+        .output()
+        .unwrap()
+        .stdout;
+
+    let candidates = String::from_utf8(get_candidates)
+        .unwrap()
+        .lines()
+        .map(|line| line.to_string())
+        .collect::<Vec<String>>();
+
+    println!("{:?}", &candidates);
+
+    for candidate in &candidates {
+        if candidate.starts_with(current) {
+            completions.push(CompletionCandidate::new(candidate));
+        }
+    }
+
+    completions
+}
 
 pub fn generate_completion() -> Result<(), FypmError> {
     let outdir = match std::env::var_os("OUT_DIR") {
