a7e97f0b810a5724e9dc81940f655f21ecd06879
Dev Myna <var.devmyna@gmail.com>
2024-11-13 10:29:58 -0300

    refactor: organize components to an appropriate file (#82)

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,32 +1,8 @@
-use wasm_bindgen_futures::spawn_local;
-use yew::prelude::*;
-
 pub const API_PORT: u16 = 8000;
 
 mod request;
-
-#[function_component(App)]
-fn app() -> Html {
-    let time_list = use_state(|| html! { <div id="time-list"> {"Loading list..."} </div> });
-
-    {
-        let time_list = time_list.clone();
-
-        use_effect_with((), move |_| {
-            spawn_local(async move {
-                get_time_list(time_list).await;
-            });
-            || ()
-        })
-    }
-
-    html! {
-        <div class="bg-black text-white font-normal">
-            {(*time_list).clone()}
-        </div>
-    }
-}
+mod components;
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<components::App>::new().render();
 }
