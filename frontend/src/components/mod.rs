446129d4596f8822482ab38138d2f115f539d6bc
Dev Myna <var.devmyna@gmail.com>
2024-11-14 18:54:42 -0300

    refactor: remove `App` from `components/mod.rs` and create new modules (#82)

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,27 +1,5 @@
-use yew::prelude::*;
-use yew::{use_effect_with, Html};
-use wasm_bindgen_futures::spawn_local;
-
-use crate::request::time::get_time_list;
-
-#[function_component(App)]
-pub fn app() -> Html {
-    let time_list = use_state(|| html! { <div id="time-list"> {"Loading list..."} </div> });
-
-    {
-        let time_list = time_list.clone();
-
-        use_effect_with((), move |_| {
-            spawn_local(async move {
-                get_time_list(time_list).await;
-            });
-            || ()
-        })
-    }
-
-    html! {
-        <div class="bg-black text-white font-normal">
-            {(*time_list).clone()}
-        </div>
-    }
-}
+pub mod layout;
+pub mod sidebar;
+pub mod time;
+pub mod app;
+pub mod summary;
